66009ffc10ae47d691a054d2fc09e83950973d49
abulabadabula <168703458+abulabadabula@users.noreply.github.com>
2026-08-24 19:41:09 +1200

    Comment out console log for environment variables
    Comment out the console log for Vite environment variables.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("🔥 Vite 环境变量:", import.meta.env);
+// console.log("🔥 Vite 环境变量:", import.meta.env);
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
